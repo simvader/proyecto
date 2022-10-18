2731f76719e535237aec792831d2c30ce89b078b
Simbad Fernandes <simbadfernandes@gmail.com>
2022-10-18 15:12:39 -0300

    updating proyect

--- movies/movie/models.py
+++ movies/movie/models.py
@@ -1,3 +1,4 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
@@ -5,3 +6,13 @@ from django.db import models
 class Movie(models.Model):
     name = models.CharField(max_length=100)
     comment = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Director(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name + " " +self.last_name
--- movies/movie/views.py
+++ movies/movie/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-
+from .models import Movie, Director
 # Create your views here.
 
 
 def home(request):
+    movie_list = Movie.objects.all()
+    context = {
+        "movies": movie_list
+    }
     return render(
         request,
-        'index.html'
+        'index.html',
+        context
     )
