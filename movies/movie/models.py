0e1a32e1f076b031977fe1b134889db2de8e4721
Simbad Fernandes <simbadfernandes@gmail.com>
2022-10-26 16:03:21 -0300

    actualizacion 26 octubre

--- movies/movie/models.py
+++ movies/movie/models.py
@@ -3,11 +3,17 @@ from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
-
 class Movie(models.Model):
     name = models.CharField(max_length=100)
     comment = models.TextField()
     imdb = models.IntegerField(default=1)
+    director = models.ForeignKey(
+        'Director',
+        on_delete=models.CASCADE
+    )
+    actores = models.ManyToManyField(
+        'Actor'
+    )
 
     def __str__(self):
         return self.name
@@ -18,3 +24,12 @@ class Director(models.Model):
 
     def __str__(self):
         return self.name + " " +self.last_name
+
+
+class Actor(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name} {self.last_name}'
